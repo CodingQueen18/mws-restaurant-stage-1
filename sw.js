dbfb8bb99bb7f12451abca5df5913240350e00ec
CodingQueen18 <33010018+CodingQueen18@users.noreply.github.com>
2018-08-31 21:56:25 -0400

    updated for offline viewing

--- sw.js
+++ sw.js
@@ -1,10 +1,47 @@
+const cacheName = 'v1';
+const urlsToCache =
+['css/styles.css',
+'js/dbhelper.js',
+'js/main.js',
+'js/restaurant_info.js',
+'index.html',
+'restaurant.html'];
 /*===============Install The Service Worker==========*/
 self.addEventListener('install', (e) => {
   console.log('Service Worker Installed');
+  //Open Cache
+  e.waitUntil(
+    caches.open(cacheName)
+    .then(cache => {
+      console.log('Service Worker File Cached');
+      cache.addAll(urlsToCache);
+    })
+  )
 });
 
 
 /*================Activate The Service Worker=============*/
 self.addEventListener('activate', (e) => {
   console.log('Service Worker Activated');
+  //Remove Old Cache
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if(cache != cacheName) {
+            console.log('Clear Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
+
+/*================Offline Viewing==================*/
+self.addEventListener('fetch', e => {
+  console.log('Fetching');
+  e.respondWith(
+    fetch(e.request)
+    .catch(() => caches.match(e.request)));
+})
